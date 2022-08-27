3830404d4dceb5709b8d73fa714d9fcac86c2f1e
George Baptista <george@omame.com>
2022-08-27 22:33:20 +0900

    chp06-pod-lifecycle/set623_liveness_probe/

---
+++ chp06-pod-lifecycle/set623_liveness_probe/README.md
@@ -0,0 +1,15 @@
+# 6.2.3 Creating an HTTP GET liveness probe
+
+## Objectives
+1. Check the container's health using liveness probes
+2. Add liveness probes to each of the containers in the `kiada-ssl` pod
+---
+
+### Notes
+
+**Types of Liveness Probes**
+
+1. An HTTP GET probe
+2. TCP Socket probe
+3. Exec probe
+
---
+++ chp06-pod-lifecycle/set623_liveness_probe/RUN.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+source ../../common/setup.sh
+FULLPATH=$(pwd)
+
+echo $HR_TOP
+
+echo "kubectl apply -f $FULLPATH/set623_namespace.yaml"
+kubectl apply -f $FULLPATH/set623_namespace.yaml
+echo $HR
+
+echo "In a separate terminal, run this"
+echo "kubectl get events -w -n=chp06-set623"
+
+enter_no_clear
+
+echo "kubectl apply -f $FULLPATH/set623_pod.yaml"
+kubectl apply -f $FULLPATH/set623_pod.yaml
+echo $HR
+
+echo "kubectl wait --for=condition=Ready=True pod liveness -n=chp06-set623 --timeout=120s"
+kubectl wait --for=condition=Ready=True pod kiada-liveness -n=chp06-set623 --timeout=120s
+echo ""
+
+echo "The only indication that Kubernetes is executing the probe is found in the container logs."
+
+echo $HR
+echo "kubectl logs kiada-liveness -c kiada  -n=chp06-set623"
+echo ""
+kubectl logs kiada-liveness -c kiada -n=chp06-set623
+enter
+
+echo "kubectl exec kiada-liveness -n=chp06-set623 -c envoy -- tail /tmp/envoy.admin.log"
+echo ""
+kubectl exec kiada-liveness -n=chp06-set623 -c envoy -- tail /tmp/envoy.admin.log
+enter
+
+
+echo "kubectl get all -n=chp06-set623 -o wide"
+kubectl get all -n=chp06-set623 -o wide
+echo $HR
+
+echo "IP=\$(kubectl get pod -n=chp06-set623 -o jsonpath='{.items[0].status.podIP}')"
+IP=$(kubectl get pod -n=chp06-set623 -o jsonpath='{.items[0].status.podIP}')
+echo $IP
+echo $HR
+
+
+echo "Make the healthness test fail"
+echo "curl -X POST $IP:9901/healthcheck/fail"
+echo "Press <ENTER> to start the crash"
+enter_no_clear
+echo "curl -X POST $IP:9901/healthcheck/fail"
+curl -X POST $IP:9901/healthcheck/fail
+echo "curl -X POST $IP:9901/healthcheck/fail"
+curl -X POST $IP:9901/healthcheck/fail
+echo "curl -X POST $IP:9901/healthcheck/fail"
+curl -X POST $IP:9901/healthcheck/fail
+echo "curl -X POST $IP:9901/healthcheck/fail"
+curl -X POST $IP:9901/healthcheck/fail
+echo "curl -X POST $IP:9901/healthcheck/fail"
+curl -X POST $IP:9901/healthcheck/fail
+
+enter_no_clear
+
+
+echo "Make the liveness probe succeed again"
+echo "curl -X POST $IP:9901/healthcheck/ok"
+curl -X POST $IP:9901/healthcheck/ok
+echo $HR
+
+echo "kubectl wait --for=condition=Ready=True pod liveness -n=chp06-set623 --timeout=120s"
+kubectl wait --for=condition=Ready=True pod kiada-liveness -n=chp06-set623 --timeout=120s
+echo ""
+
+
+echo "kubectl get pod kiada-liveness -n=chp06-set623"
+kubectl get pod kiada-liveness -n=chp06-set623
+enter 
+
+
+echo "kubectl describe pod kiada-liveness -n=chp06-set623"
+kubectl describe pod kiada-liveness -n=chp06-set623
+echo $HR
+
+
+echo "kubectl delete ns chp06-set623"
+kubectl delete ns chp06-set623
+
+echo $HR_TOP
