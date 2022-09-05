8ea262e2441811000406cd6c2e2a3a2dc9f6bd61
George Baptista <george@omame.com>
2022-09-05 23:25:11 +0900

    Added chp08-persistent-volumes/set833_custom_storageClass/

---
+++ chp08-persistent-volumes/set833_custom_storageClass/README.md
@@ -0,0 +1,10 @@
+# 8.3.2 Dynamic provisioning using the default storage class
+
+### Objective
+
+1. Explore dynamic provisioning using the default storage class
+
+### Notes
+
+* We don't have to manually provision any PersistentVolumes here.
+
---
+++ chp08-persistent-volumes/set833_custom_storageClass/RUN.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+source ../../common/setup.sh
+FULLPATH=$(pwd)
+
+echo $HR_TOP
+
+#####
+
+echo "Check version of microk8s installed:"
+echo "snap info microk8s | grep tracking"
+snap info microk8s | grep tracking
+echo $HR
+
+echo "Before running anything, make sure we don't have any PV or PVC objects running in cluster."
+
+NS=chp08-set833
+
+echo "kubectl delete ns $NS"
+kubectl delete ns $NS
+echo $HR 
+
+PV=$(kubectl get pv -o=name)
+
+if [ $PV ]
+then
+	echo "PV exists: $PV"
+	echo "kubectl patch $PV -p '{metadata:{finalizers:null}}'"
+	kubectl patch $PV -p '{"metadata":{"finalizers":null}}'
+	echo ""
+	echo "kubectl delete pv $PV --force --grace-period=0"
+	kubectl delete $PV --force --grace-period=0
+else
+	echo "PV does not exist."
+fi
+
+echo $HR
+
+PVC=$(kubectl get pvc -o=name -n=$NS)
+
+if [ $PVC ]
+then
+	echo "PVC exists: $PVC"
+	echo $HR
+	echo "kubectl patch $PVC -n=$NS -p '{metadata:{finalizers:null}}'"
+	kubectl patch $PVC -n=$NS -p '{"metadata":{"finalizers":null}}'
+	echo ""
+	echo "kubectl delete $PVC -n=$NS --force --grace-period=0"
+	kubectl delete $PVC -n=$NS --force --grace-period=0
+else
+	echo "PVC does not exist."
+fi
+
+echo $HR
+
+#####
+
+echo "Check the StorageClass used. In particular, pay attention to the 'volumeBindingMode' field:"
+echo "kubectl get sc"
+kubectl get sc
+echo $HR
+
+echo "kubectl get sc fast -o yaml"
+kubectl get sc fast -o yaml
+echo $HR
+
+echo "kubectl get sc fast -o=jsonpath='{.items[0].volumeBindingMode}'"
+kubectl get sc fast -o=jsonpath='{.items[0].volumeBindingMode}'
+enter
+#####
+
+echo "kubectl get pv"
+kubectl get pv
+enter
+
+
+kubectl apply -f $FULLPATH/set833_namespace.yaml
+kubectl apply -f $FULLPATH/set833_storage_class.yaml
+kubectl apply -f $FULLPATH/set833_pvc.yaml
+
+echo $HR 
+
+enter
+
+echo "In a microk8s cluster, the persistent volume claim we create here is not bound immediately, and its status should be 'Pending'."
+echo "Here, our claim will remain in the Pending state until we create a pod that uses this claim."
+echo ""
+echo "kubectl get pvc/quiz-data-fast -n=chp08-set833"
+kubectl get pvc/quiz-data-fast -n=chp08-set833
+enter
+
+echo "kubectl describe pvc/quiz-data-fast -n=chp08-set833"
+kubectl describe pvc/quiz-data-fast -n=chp08-set833
+
+enter
+
+echo "Apply pod yaml here:"
+echo "kubectl apply -f $FULLPATH/set833_pod-default.yaml"
+kubectl apply -f $FULLPATH/set833_pod-default.yaml
+echo $HR 
+
+echo "kubectl wait --for=condition=Ready=True pods/quiz-default -n=chp08-set833 --timeout=120s"
+kubectl wait --for=condition=Ready=True pods/quiz-default -n=chp08-set833 --timeout=120s
+echo $HR
+
+enter
+
+echo "We now should have a dynamically provisioned PV:"
+echo ""
+echo "kubectl get pv"
+kubectl get pv
+enter
+
+echo "kubectl describe pv"
+kubectl describe pv
+enter
+
+echo "kubectl get events -n=chp08-set833"
+kubectl get events -n=chp08-set833
+echo $HR
+
+echo "Press enter to delete objects"
+enter
+
+#####
+
+echo "kubectl delete ns chp08-set833"
+kubectl delete ns chp08-set833
+echo $HR
+
+PV=$(kubectl get pv -o=name)
+echo "PV exists: $PV"
+echo "kubectl patch $PV -p '{"metadata":{"finalizers":null}}'"
+kubectl patch $PV -p '{"metadata":{"finalizers":null}}'
+echo ""
+# echo "kubectl delete pv $PV"
+# kubectl delete $PV
+echo "kubectl delete pv $PV --force --grace-period=0"
+kubectl delete $PV --force --grace-period=0
+
+
+echo $HR_TOP
