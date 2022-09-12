3e4c8bd6ac5e09e271a21a066f17688451271dde
George Baptista <george@omame.com>
2022-09-12 18:38:25 +0900

    chp10_namespaces_labels/set1022_labels/

---
+++ chp10_namespaces_labels/set1022_labels/RUN.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+source ../../common/setup.sh
+FULLPATH=$(pwd)
+
+echo $HR_TOP
+
+echo "Create namespace first"
+kubectl apply -f $FULLPATH/set1022_namespace.yaml
+echo $HR
+
+echo "Next, create objects"
+kubectl apply -f $FULLPATH/kiada --recursive
+echo ""
+
+kubectl apply -f $FULLPATH/quiz --recursive
+echo ""
+
+kubectl apply -f $FULLPATH/quote --recursive
+echo $HR 
+
+echo "kubectl wait --for=condition=Ready=True pods/kiada-001 -n=chp10-set1022 --timeout=120s"
+kubectl wait --for=condition=Ready=True pods/kiada-001 -n=chp10-set1022 --timeout=120s
+echo $HR 
+
+kubectl wait --for=condition=Ready=True pods -n=chp10-set1022
+
+enter
+
+echo "kubectl describe pod/kiada-001 -n=chp10-set1022 | head"
+kubectl describe pod/kiada-001 -n=chp10-set1022 | head
+enter
+
+
+echo "kubectl get pods --show-labels -n=chp10-set1022 | sort"
+kubectl get pods --show-labels -n=chp10-set1022 | sort
+enter
+
+
+echo "kubectl get pods -L app,rel -n=chp10-set1022 | sort"
+kubectl get pods -L app,rel -n=chp10-set1022 | sort
+
+enter
+
+echo "Label all objects of a kind, in imperative style."
+echo "kubectl label pod --all suite=kiada-suite --overwrite -n=chp10-set1022"
+kubectl label pod --all suite=kiada-suite --overwrite -n=chp10-set1022 
+echo $HR
+
+echo "kubectl get pods --show-labels -n=chp10-set1022  | sort"
+kubectl get pods --show-labels -n=chp10-set1022  | sort
+
+enter
+
+
+echo "Remove label from objects, in imperative style."
+echo "kubectl label pod --all suite- --overwrite -n=chp10-set1022"
+kubectl label pod --all suite- --overwrite -n=chp10-set1022 
+echo $HR
+
+echo "kubectl get pods --show-labels -n=chp10-set1022  | sort"
+kubectl get pods --show-labels -n=chp10-set1022  | sort
+echo $HR 
+
+
+echo "kubectl delete ns chp10-set1022"
+kubectl delete ns chp10-set1022
+
+echo $HR_TOP
