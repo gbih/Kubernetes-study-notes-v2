cdcb1288476c45cb02096b1530c76ea4b117362a
George Baptista <george@omame.com>
2022-09-12 16:41:25 +0900

    chp10_namespaces_labels/set1012_create_namespaces

--- chp10_namespaces_labels/set1012_create_namespaces/README.md
+++ chp10_namespaces_labels/set1012_create_namespaces/README.md
@@ -1,17 +1,5 @@
-# 9.1.1 Setting the command and arguments
+# 10.1.2 Creatingnamespaces
 
 ### Objective
 
-1. Make the kiada applicatio configurable via command-line arguments and environment variables.
-
-## Build and push image to Docker Hub
-
-Make sure you have logged into Docker Hub from within the (Multipass) node.
-
-```
-docker build -t georgebaptista/kiada:0.4 ./
-
-docker images
-
-docker push georgebaptista/kiada:0.4
-```
+1. Create namespaces both imperatively on CLI, and declaratively via YAML manifest file.
--- chp10_namespaces_labels/set1012_create_namespaces/RUN.sh
+++ chp10_namespaces_labels/set1012_create_namespaces/RUN.sh
@@ -4,17 +4,27 @@ FULLPATH=$(pwd)
 
 echo $HR_TOP
 
+echo "Create namespace imperatively via CLI."
+echo "kubectl create namespace kiada-tes1"
+kubectl create namespace kiada-test1
+echo $HR 
+
+echo "Create namespace declaratively via YAML manifest file."
+echo "kubectl apply -f $FULLPATH/set1012_namespace.yaml"
+kubectl apply -f $FULLPATH/set1012_namespace.yaml
+echo $HR 
+
 echo "kubectl get ns"
 kubectl get ns
 echo $HR 
 
-echo "List pods in the kube-system namespace."
-echo "kubectl get pods -n=kube-system -o wide"
-kubectl get pods -n=kube-system -o wide
-echo $HR 
 
-echo "Listing objects across all namespaces (eg ConfigMap)"
-echo "kubectl get cm -A"
-kubectl get cm -A
+
+echo "kubectl delete ns kiada-test1"
+kubectl delete ns kiada-test1
+echo ""
+
+echo "kubectl delete ns kiada-test2"
+kubectl delete ns kiada-test2
 
 echo $HR_TOP
