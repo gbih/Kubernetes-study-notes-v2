c38be1b96496698fc8f3bc4dc9b79fdcdd46e8c9
George Baptista <george@omame.com>
2022-09-14 10:33:12 +0900

    Edited chp08-persistent-volumes/set800_change_default_class/RUN_SET_STORAGE_PATH.sh

---
+++ chp08-persistent-volumes/set800_change_default_class/RUN_SET_STORAGE_PATH.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+source ../../common/setup.sh
+FULLPATH=$(pwd)
+
+echo $HR_TOP
+
+# Set the default StorageClass on microk8s
+
+target_storage_class='openebs-jiva-csi-default'
+#target_storage_class='openebs-hostpath'
+#target_storage_class='microk8s-hostpath'
+
+#####
+
+kubectl get sc | sort
+
+echo $HR 
+
+echo "Determine which StorageClass is marked as default:"
+
+# Split on what is returned by grep, 
+# Then we use the 'cut' command to essentially split:
+# -f (field number), so here we want the first slice
+# cut uses tab as a default field delimiter, so we need to change to space:
+# -d option uses space as a field separator or delimiter
+# https://www.geeksforgeeks.org/cut-command-linux-examples/
+
+current_storage_class=$(kubectl get sc | grep '(default)' | cut -d ' ' -f 1)
+echo $current_storage_class
+echo $HR
+
+mark_non_default () {
+	current_sc=$1
+	echo "Mark the default StorageClass $current_sc as non-default:"
+	kubectl patch sc $current_sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+}
+
+mark_default () {
+	target_sc=$1
+	echo "Mark $target_sc StorageClass as default:"
+	kubectl patch storageclass $target_sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+}
+
+if [ $current_storage_class != $target_storage_class ]
+then
+	echo "Current StorageClass is not $target_storage_class."
+	mark_non_default $current_storage_class
+	echo $HR
+	mark_default $target_storage_class
+	echo $HR
+	kubectl get sc | sort
+
+else
+	echo "Current StorageClass is already $target_storage_class, no need to change."
+fi
+
+echo $HR_TOP
