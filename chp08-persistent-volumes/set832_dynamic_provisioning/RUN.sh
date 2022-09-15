42549a5916a90a58225686005f8a0c7551ab6f90
George Baptista <george@omame.com>
2022-09-15 22:46:20 +0900

    Edited chp08-persistent-volumes/set832_dynamic_provisioning

--- chp08-persistent-volumes/set832_dynamic_provisioning/RUN.sh
+++ chp08-persistent-volumes/set832_dynamic_provisioning/RUN.sh
@@ -11,79 +11,40 @@ echo "snap info microk8s | grep tracking"
 snap info microk8s | grep tracking
 echo $HR
 
-echo "Before running anything, make sure we don't have any PV or PVC objects running in the cluster."
-
-NS=chp08-set832
-
-echo "kubectl delete ns $NS"
-kubectl delete ns $NS
-echo $HR 
-
-PV=$(kubectl get pv -o=name)
-
-if [ $PV ]
-then
-	echo "PV exists: $PV"
-	echo "kubectl patch $PV -p '{metadata:{finalizers:null}}'"
-	kubectl patch $PV -p '{"metadata":{"finalizers":null}}'
-	echo ""
-	echo "kubectl delete pv $PV --force --grace-period=0"
-	kubectl delete $PV --force --grace-period=0
-else
-	echo "PV does not exist."
-fi
-
-echo $HR
-
-PVC=$(kubectl get pvc -o=name -n=$NS)
-
-if [ $PVC ]
-then
-	echo "PVC exists: $PVC"
-	echo $HR
-	echo "kubectl patch $PVC -n=$NS -p '{metadata:{finalizers:null}}'"
-	kubectl patch $PVC -n=$NS -p '{"metadata":{"finalizers":null}}'
-	echo ""
-	echo "kubectl delete $PVC -n=$NS --force --grace-period=0"
-	kubectl delete $PVC -n=$NS --force --grace-period=0
-else
-	echo "PVC does not exist."
-fi
-
-echo $HR
-
-#####
-
 echo "Check the StorageClass used. In particular, pay attention to the 'volumeBindingMode' field:"
 echo "kubectl get sc"
 kubectl get sc
 echo $HR
 
+echo "Make sure this namespace is clear beforehand"
+
+NS=$(kubectl get ns/chp08-set832 -o name)
+if [ $NS ==  'namespace/chp08-set832' ]
+then
+	kubectl delete ns chp08-set832
+fi
 
 
-echo "kubectl get pv"
-kubectl get pv
-enter
-
-echo "kubectl apply -f $FULLPATH/set832_namespace.yaml"
-kubectl apply -f $FULLPATH/set832_namespace.yaml
 echo $HR
 
-echo "kubectl apply -f $FULLPATH/set832_pvc.yaml"
+kubectl apply -f $FULLPATH/set832_namespace.yaml
 kubectl apply -f $FULLPATH/set832_pvc.yaml
 echo $HR 
-
-
 
 echo "In a microk8s cluster, the persistent volume claim we create here is not bound immediately, and its status should be 'Pending'."
 echo "Here, our claim will remain in the Pending state until we create a pod that uses this claim."
 echo ""
-echo "kubectl get pvc/quiz-data-default -n=chp08-set832"
-kubectl get pvc/quiz-data-default -n=chp08-set832
+echo "kubectl get pvc/quiz-data-default -n=chp08-set832 -o wide"
+kubectl get pvc/quiz-data-default -n=chp08-set832 -o wide
 enter
 
 echo "kubectl describe pvc/quiz-data-default -n=chp08-set832"
 kubectl describe pvc/quiz-data-default -n=chp08-set832
+echo $HR
+
+echo "Check persistent volumes"
+echo "kubectl get pv -o wide"
+kubectl get pv -o wide
 
 enter
 
@@ -94,7 +55,6 @@ echo $HR
 
 echo "kubectl wait --for=condition=Ready=True pods/quiz-default -n=chp08-set832 --timeout=360s"
 kubectl wait --for=condition=Ready=True pods/quiz-default -n=chp08-set832 --timeout=360s
-echo $HR
 
 enter
 
@@ -111,21 +71,11 @@ enter_delete
 
 #####
 
-echo "kubectl delete pvc --all -n=chp08-set833" 
-kubectl delete pvc --all -n=chp08-set833 
-echo $HR
-
+echo "Here, PVs are provisioned dynamically via PVCs. Hence deleting the namespace will clear both PVC and PV automatically."
+echo ""
 echo "kubectl delete ns chp08-set832"
 kubectl delete ns chp08-set832
 echo $HR
-
-# PV=$(kubectl get pv -o=name)
-# echo "PV exists: $PV"
-# echo "kubectl patch $PV -p '{"metadata":{"finalizers":null}}'"
-# kubectl patch $PV -p '{"metadata":{"finalizers":null}}'
-# echo ""
-# echo "kubectl delete pv $PV --force --grace-period=0"
-# kubectl delete $PV --force --grace-period=0
 
 
 echo $HR_TOP
