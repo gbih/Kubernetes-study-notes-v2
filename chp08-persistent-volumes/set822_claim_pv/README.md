f4ed1469eaa56a737fe02538547dceb23bc2655b
George Baptista <george@omame.com>
2022-09-15 21:14:44 +0900

    Edited chp08-persistent-volumes/set822_claim_pv/

--- chp08-persistent-volumes/set822_claim_pv/README.md
+++ chp08-persistent-volumes/set822_claim_pv/README.md
@@ -7,18 +7,3 @@
 ### Notes
 
 * Our cluster has two PVs, but before we can use them, we have to claim them via a PVC.
-
-### Events
-
-```
-kubectl get events -n=chp08-set822                                                                         main: Thu Sep 15 14:14:51 2022
-
-LAST SEEN   TYPE     REASON      OBJECT     MESSAGE
-2m14s       Normal   Scheduled   pod/quiz   Successfully assigned chp08-set822/quiz to worker1
-2m12s       Normal   Pulled      pod/quiz   Container image "luksa/quiz-api:0.1" already present on machine
-2m12s       Normal   Created     pod/quiz   Created container quiz-api
-2m12s       Normal   Started     pod/quiz   Started container quiz-api
-2m12s       Normal   Pulled      pod/quiz   Container image "mongo:5" already present on machine
-2m12s       Normal   Created     pod/quiz   Created container mongo
-2m11s       Normal   Started     pod/quiz   Started container mongo
-```
--- chp08-persistent-volumes/set822_claim_pv/RUN.sh
+++ chp08-persistent-volumes/set822_claim_pv/RUN.sh
@@ -4,48 +4,24 @@ FULLPATH=$(pwd)
 
 echo $HR_TOP
 
-echo "This cluster has two PVs. Before the quiz pod can use the quiz-data volume, we need to claim it, via a PVC."
+echo "This cluster has two PVs. Before the quiz pod can use the quiz-data volume, we need to claim it via a PVC."
 
 echo $HR 
 
 kubectl apply -f $FULLPATH/set822_namespace.yaml
-kubectl apply -f $FULLPATH/set822_pod.yaml
+kubectl apply -f $FULLPATH/set822_pvc_quizdata.yaml
 kubectl apply -f $FULLPATH/set822_pv_quizdata.yaml
 kubectl apply -f $FULLPATH/set822_pv_otherdata.yaml
-kubectl apply -f $FULLPATH/set822_pvc.yaml
 
 echo $HR
-
-# Still not possible to pass arbitrary jsonpath to kubectl wait?
-# https://github.com/kubernetes/kubernetes/issues/83094
-# echo "Wait for PV quiz-data status to be Available:"
-# until kubectl get pv/quiz-data --output=jsonpath='{.status.phase}' | grep "Available"; do : echo "PV Status: " kubectl get pv/quiz-data --output=jsonpath='{.status.phase}'; done
-# echo $HR 
-
-# echo "Wait for PV other-data status to be Available:"
-# until kubectl get pv/other-data --output=jsonpath='{.status.phase}' | grep "Available"; do : echo "PV Status: " kubectl get pv/quiz-other --output=jsonpath='{.status.phase}'; done
-# echo $HR 
 
 echo "kubectl get pv -o wide"
 kubectl get pv -o wide
 
 enter
 
-value=$(<set822_pvc.yaml)
-echo "Check the PVC yaml file:"
-echo ""
-echo "$value"
-enter
-
-
-
-echo "Wait for PVC status to be Bound:"
-until kubectl get pvc/quiz-data -n=chp08-set822 --output=jsonpath='{.status.phase}' | grep "Bound"; do : kubectl get pvc/quiz-data -n=chp08-set822 --output=jsonpath='{.status.phase}' ; done
-echo $HR 
-
-
-echo "kubectl get pvc/quiz-data -n=chp08-set822"
-kubectl get pvc -n=chp08-set822
+echo "kubectl get pvc/quiz-data -n=chp08-set822 -o wide"
+kubectl get pvc -n=chp08-set822 -o wide
 echo $HR
 
 echo "kubectl describe pvc/quiz-data -n=chp08-set822"
@@ -56,34 +32,17 @@ enter
 echo "kubectl get pvc/quiz-data -n=chp08-set822 -o yaml"
 kubectl get pvc/quiz-data -n=chp08-set822 -o yaml
 
-enter
-
-echo "kubectl wait --for=condition=Ready=True pods/quiz -n=chp08-set822 --timeout=120s"
-kubectl wait --for=condition=Ready=True pods/quiz -n=chp08-set822 --timeout=120s
-echo $HR
-
-echo "kubectl get all -n=chp08-set822 -o wide"
-kubectl get all -n=chp08-set822 -o wide
-echo $HR
-
-echo "kubectl get events -n=chp08-set822"
-kubectl get events -n=chp08-set822
-
 enter_delete
-
-echo "kubectl delete pvc quiz-data -n=chp08-set822"
-kubectl delete pvc quiz-data -n=chp08-set822
-echo $HR
-
-echo "kubectl delete pv quiz-data"
-kubectl delete pv quiz-data
-echo $HR
-
-echo "kubectl delete pv other-data"
-kubectl delete pv other-data
-echo $HR
 
 echo "kubectl delete ns chp08-set822"
 kubectl delete ns chp08-set822
+echo $HR 
+
+echo "Deleting the PVC will not delete the PV, since we originally creted the PV beforehand"
+echo "So, we need to also manually delete the PVs here:"
+
+echo "kubectl delete $(kubectl get pv -o name)"
+echo ""
+kubectl delete $(kubectl get pv -o name)
 
 echo $HR_TOP
