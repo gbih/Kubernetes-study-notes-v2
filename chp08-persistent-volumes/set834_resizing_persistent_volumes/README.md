630118eb500be8a7e4f7db88ef0a1eb237c39842
George Baptista <george@omame.com>
2022-09-16 09:22:55 +0900

    Edited chp08-persistent-volumes/set834_resizing_persistent_volumes

--- chp08-persistent-volumes/set834_resizing_persistent_volumes/README.md
+++ chp08-persistent-volumes/set834_resizing_persistent_volumes/README.md
@@ -5,6 +5,34 @@
 1. Explore how to resize persistent volumes
 2. Apparently neither microk8s-hostpath nor openebs-hostpath StorageClass is capable of expanding the persistent volume.
 
-### To-dp
 
-1. Find a provisioner that allows expanding the persistent volume.
+### Notes
+
+* Requesting a different volume size does not work with the microk8s provisioner. We get this error message: 
+
+``Ignoring the PVC: didn't find a plugin capable of expanding the volume; waiting for an external controller to process this PVC.
+``
+
+### To-do
+
+* Find a provisioner that allows expanding the persistent volume.
+
+
+### Events
+
+```
+kubectl get events -n=chp08-set834
+LAST SEEN   TYPE      REASON                  OBJECT                                    MESSAGE
+76s         Normal    WaitForFirstConsumer    persistentvolumeclaim/quiz-data-default   waiting for first consumer to be created before binding
+75s         Normal    Provisioning            persistentvolumeclaim/quiz-data-default   External provisioner is provisioning volume for claim "chp08-set834/quiz-data-default"
+73s         Normal    ExternalProvisioning    persistentvolumeclaim/quiz-data-default   waiting for a volume to be created, either by external provisioner "microk8s.io/hostpath" or manually created by system administrator
+70s         Normal    ProvisioningSucceeded   persistentvolumeclaim/quiz-data-default   Successfully provisioned volume pvc-167a31fb-5f5c-4eca-9345-9d0c9764d0ba
+69s         Normal    Scheduled               pod/quiz-default                          Successfully assigned chp08-set834/quiz-default to worker1
+68s         Normal    Pulled                  pod/quiz-default                          Container image "georgebaptista/quiz-api:0.1" already present on machine
+68s         Normal    Created                 pod/quiz-default                          Created container quiz-api
+68s         Normal    Started                 pod/quiz-default                          Started container quiz-api
+68s         Normal    Pulled                  pod/quiz-default                          Container image "mongo:5" already present on machine
+68s         Normal    Created                 pod/quiz-default                          Created container mongo
+68s         Normal    Started                 pod/quiz-default                          Started container mongo
+65s         Warning   ExternalExpanding       persistentvolumeclaim/quiz-data-default   Ignoring the PVC: didn't find a plugin capable of expanding the volume; waiting for an external controller to process this PVC.
+```
--- chp08-persistent-volumes/set834_resizing_persistent_volumes/RUN.sh
+++ chp08-persistent-volumes/set834_resizing_persistent_volumes/RUN.sh
@@ -11,70 +11,27 @@ echo "snap info microk8s | grep tracking"
 snap info microk8s | grep tracking
 echo $HR
 
-echo "Before running anything, make sure we don't have any PV or PVC objects running in cluster."
+echo "Make sure this namespace is clear beforehand"
 
-NS=chp08-set834
+NS=$(kubectl get ns/chp08-set834 -o name)
+if [ $NS == 'namespace/chp08-set834' ]
+then
+	kubectl delete ns chp08-set834
+fi
 
-echo "kubectl delete ns $NS"
-kubectl delete ns $NS
 echo $HR 
 
-PV=$(kubectl get pv -o=name)
-
-if [ $PV ]
-then
-	echo "PV exists: $PV"
-	echo "kubectl patch $PV -p '{metadata:{finalizers:null}}'"
-	kubectl patch $PV -p '{"metadata":{"finalizers":null}}'
-	echo ""
-	echo "kubectl delete pv $PV --force --grace-period=0"
-	kubectl delete $PV --force --grace-period=0
-else
-	echo "PV does not exist."
-fi
-
-echo $HR
-
-PVC=$(kubectl get pvc -o=name -n=$NS)
-
-if [ $PVC ]
-then
-	echo "PVC exists: $PVC"
-	echo $HR
-	echo "kubectl patch $PVC -n=$NS -p '{metadata:{finalizers:null}}'"
-	kubectl patch $PVC -n=$NS -p '{"metadata":{"finalizers":null}}'
-	echo ""
-	echo "kubectl delete $PVC -n=$NS --force --grace-period=0"
-	kubectl delete $PVC -n=$NS --force --grace-period=0
-else
-	echo "PVC does not exist."
-fi
-
-echo $HR
-
-#####
-
-
+echo "Check the StorageClass used. In particular, pay attention to the 'volumeBindingMode' field:"
 echo "kubectl get sc"
 kubectl get sc
-echo $HR
 
-echo "kubectl get pv"
-kubectl get pv
 enter
 
-echo "kubectl apply -f $FULLPATH/set834_namespace.yaml"
+kubectl apply -f $FULLPATH/microk8s-hostpath.yaml
 kubectl apply -f $FULLPATH/set834_namespace.yaml
-echo $HR
+kubectl apply -f $FULLPATH/set834_pvc_original.yaml
 
-echo "kubectl apply -f $FULLPATH/set834_pvc.yaml"
-kubectl apply -f $FULLPATH/set834_pvc.yaml
 echo $HR 
-
-echo "kubectl get pvc/quiz-data-default -n=chp08-set834"
-kubectl get pvc/quiz-data-default -n=chp08-set834
-enter
-
 
 echo "Apply pod yaml here:"
 echo "kubectl apply -f $FULLPATH/set834_pod-default.yaml"
@@ -91,53 +48,32 @@ echo "We now should have a dynamically provisioned PV:"
 echo ""
 echo "kubectl get pv"
 kubectl get pv
-enter
-
-
-echo "kubectl delete pod/quiz-default -n=chp08-set834"
-kubectl delete pod/quiz-default -n=chp08-set834
-enter
-
-
-echo "Requesting a larger volume via a new PVC"
-#echo "It appears that the default microk8s-hostpath storageclass that provisions the pvc does not support resize."
-echo "kubectl apply -f $FULLPATH/set834_pvc_resize.yaml"
-kubectl apply -f $FULLPATH/set834_pvc_resize.yaml
-
-enter
-
-echo "Apply pod yaml here:"
-echo "kubectl apply -f $FULLPATH/set834_pod-default.yaml"
-kubectl apply -f $FULLPATH/set834_pod-default.yaml
-echo $HR 
-
-echo "kubectl wait --for=condition=Ready=True pods/quiz-default -n=chp08-set834 --timeout=360s"
-kubectl wait --for=condition=Ready=True pods/quiz-default -n=chp08-set834 --timeout=360s
 echo $HR
-
-enter
 
 echo "kubectl get pvc/quiz-data-default -n=chp08-set834"
 kubectl get pvc/quiz-data-default -n=chp08-set834
-enter
+echo $HR 
 
 
-echo "kubectl describe pvc quiz-data-default -n=chp08-set834"
-kubectl describe pvc quiz-data-default -n=chp08-set834
-echo $HR
+echo "Request a larger volume via an updated PersistentVolumeClaim"
+echo "kubectl apply -f $FULLPATH/set834_pvc_resize.yaml"
+kubectl apply -f $FULLPATH/set834_pvc_resize.yaml
+echo $HR 
 
+echo "kubectl get pvc/quiz-data-default -n=chp08-set834"
+kubectl get pvc/quiz-data-default -n=chp08-set834
+echo $HR 
+
+echo "Requesting a different volume size does not work with the microk8s provisioner. We get this error message: Ignoring the PVC: didn't find a plugin capable of expanding the volume; waiting for an external controller to process this PVC."
+
+enter 
+
+echo "kubectl get events -n=chp08-set834"
+kubectl get events -n=chp08-set834
+
+enter_delete
 
 echo "kubectl delete ns chp08-set834"
 kubectl delete ns chp08-set834
-echo $HR
-
-PV=$(kubectl get pv -o=name)
-echo "PV exists: $PV"
-echo "kubectl patch $PV -p '{"metadata":{"finalizers":null}}'"
-kubectl patch $PV -p '{"metadata":{"finalizers":null}}'
-echo ""
-echo "kubectl delete pv $PV --force --grace-period=0"
-kubectl delete $PV --force --grace-period=0
-
 
 echo $HR_TOP
